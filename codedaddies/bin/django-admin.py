fac13ee1eb1852df04f4332dea7c28367f998439
Adel Cabrera <adel.cabrera.mesina@gmail.com>
2020-02-09 15:33:40 -0300

    comment

---
+++ codedaddies/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/darkonnen/Documents/Projects/codedaddies_list/codedaddies/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
